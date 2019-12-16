b8a435baa8890ce3fe56000e5e93e4ae0e1aeb8c
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2019-12-16 14:41:36 +0200

    changing view based on state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import MainView from './views/MainView';
@@ -6,19 +6,37 @@ import NavBar from './components/Nav/NavBar';
 import ProjectsView from './views/ProjectsView';
 import AboutView from './views/AboutView';
 import ContactView from './views/ContactView';
+import { render } from 'react-dom';
 
 const App: React.FC = () => {
-  const sectionRender = Math.floor(Math.random() * 4) + 1  
+
+const [state, setState] = useState([<MainView />, <AboutView />, <ProjectsView />, <ContactView />]);
+  // const viewArray = [<MainView />, <AboutView />, <ProjectsView />, <ContactView />];
+  const renderViews = state.map((item, i) => <div key={i}>{item}</div>);
+
+
+
+  // const iterateView = () => {
+  //   for (let i = 0; i < viewArray.length; i++) {
+  //     return (
+  //       <div className="App">
+  //         <NavBar />
+  //         component
+  //       </div>
+  //     )
+  //   };
+  // };
 
   return (
     <div className="App">
       <NavBar />
-      
-        <MainView />
+      <button onClick={() => setState([<AboutView />])}> </button>
+      {/* <MainView />
       
         <AboutView />
         <ProjectsView />
-        <ContactView />
+        <ContactView /> */}
+        {renderViews}
 
     </div>
   );
