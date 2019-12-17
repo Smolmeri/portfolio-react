2bff4886332e26823a7dd7f7809702f0cac5b282
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2019-12-17 14:01:31 +0200

    css fiexs and props for  modals

--- src/components/Projects/projectModal.tsx
+++ src/components/Projects/projectModal.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import './ProjectModal.css'
 
+import styled, { keyframes } from "styled-components";
+import { fadeIn } from "react-animations";
+
+
 const ProjectModal = (props) => {
 
-   const handleChange = () => {
-       props.onClick(0)
-   }
+    const handleChange = () => {
+        props.onClick(0)
+    }
+
+    const FadeInDiv = styled.div`
+    animation: .5s ${keyframes`${fadeIn}`};
+  `;
 
     return (
-        <div className='modal'>
+    
+        <FadeInDiv className='modal'>
             <div className='closeBtn' onClick={handleChange}>
                 <span className="btn1"></span>
                 <span className="btn2"></span>
             </div>
-            <p>Hello World</p>
-
-        </div>
+            <img className='modalImg' src={props.imgPath} alt="project1-large" />
+            <p>{props.desc}</p>
+            <a target='_blank' href={props.url}>{props.urlName}</a>
+        </FadeInDiv>
 
     )
 }
--- src/views/ProjectsView.tsx
+++ src/views/ProjectsView.tsx
@@ -13,6 +13,8 @@ const ProjectsView = (props) => {
         setState(0)
     }
 
+    
+
 
     return (
         <section id="projects">
@@ -20,34 +22,36 @@ const ProjectsView = (props) => {
                 <h1>Projects</h1>
                 <div className="cardsSection">
                     <div className="card" onClick={() => setState(1)}>
-                        <img src={img} alt="project1"/>
+                        <img src={img} alt="project1" />
                         <p>Foo</p>
                     </div>
                     <div className="card" onClick={() => setState(2)}>
-                        <img src={img} alt="project2"/>
+                        <img src={img} alt="project2" />
                         <p>Foo</p>
                     </div>
                     <div className="card" onClick={() => setState(3)}>
-                        <img src={img} alt="project3"/>
+                        <img src={img} alt="project3" />
                         <p>Foo</p>
                     </div>
                     <div className="card" onClick={() => setState(4)}>
-                        <img src={img} alt="project4"/>
+                        <img src={img} alt="project4" />
                         <p>Foo</p>
                     </div>
+                    
                 </div>
-                {state === 1 &&
-                <ProjectModal onClick={handleClick} />
-                }
-                {state === 2 &&
-                <ProjectModal onClick={handleClick}  />
-                }
-                {state === 3 &&
-                <ProjectModal onClick={handleClick}  />
-                }
-                {state === 4 &&
-                <ProjectModal onClick={handleClick}  />
-                }
+                    {state === 1 &&
+                        <ProjectModal onClick={handleClick} imgPath={img} desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis cum nesciunt impedit fugit amet laudantium voluptatum in, eius ipsa id earum vitae ab blanditiis tenetur eligendi odio eos ad.'} url={'https://github.com/Smolmeri'} urlName={'GITHUB'} />
+                    }
+                    {state === 2 &&
+                        <ProjectModal onClick={handleClick} imgPath={img} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis cum nesciunt impedit fugit amet laudantium voluptatum in, eius ipsa id earum vitae ab blanditiis tenetur eligendi odio eos ad."} url={'https://github.com/Smolmeri'} urlName={'GITHUB'} />
+                    }
+                    {state === 3 &&
+                        <ProjectModal onClick={handleClick} imgPath={img} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis cum nesciunt impedit fugit amet laudantium voluptatum in, eius ipsa id earum vitae ab blanditiis tenetur eligendi odio eos ad."} url={'https://github.com/Smolmeri'} urlName={'GITHUB'} />
+                    }
+                    {state === 4 &&
+                        <ProjectModal onClick={handleClick} imgPath={img} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis cum nesciunt impedit fugit amet laudantium voluptatum in, eius ipsa id earum vitae ab blanditiis tenetur eligendi odio eos ad."} url={'https://github.com/Smolmeri'} urlName={'GITLAB'}/>
+                    }
+                
             </div>
         </section>
     );
