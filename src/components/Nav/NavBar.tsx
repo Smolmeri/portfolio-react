cc385183e9d6a551dc2173bf76826f6126f8d92c
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2019-12-17 10:51:48 +0200

    added animation to navmenu with react-animations

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './NavBar.css';
 import Navigation from './Navigation';
 
+import styled, { keyframes } from "styled-components";
+import { fadeIn } from "react-animations";
+
 const NavBar = () => {
 
     const [state, setState] = useState(false);
@@ -16,6 +19,10 @@ const NavBar = () => {
 
     console.log("STATE", state);
 
+    const FadeInDiv = styled.div`
+  animation: .5s ${keyframes `${fadeIn}`};
+`;
+
     return (
         <div>
             <div onClick={openNav} className="burgerContainer">
@@ -24,7 +31,9 @@ const NavBar = () => {
                 <span className="bar3"></span>
             </div>
             <div>
+                <FadeInDiv>
                 { state && <Navigation />}
+                </FadeInDiv>
             </div>
         </div>
     )
--- src/views/ProjectsView.tsx
+++ src/views/ProjectsView.tsx
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ProjectsView.css';
 import img from '../imgPlaceholder.png'
 import ProjectModal from '../components/Projects/ProjectModal';
 
 const ProjectsView = () => {
+
+    const [state, setState] = useState(0);
+
+    console.log(state);
+
+
     return (
         <section id="projects">
             <div className="projectsSection">
                 <h1>Projects</h1>
                 <div className="cardsSection">
-                    <div className="card">
+                    <div className="card" onClick={() => setState(1)}>
                         <img src={img} alt="project1"/>
                         <p>Foo</p>
                     </div>
-                    <div className="card">
+                    <div className="card" onClick={() => setState(2)}>
                         <img src={img} alt="project2"/>
                         <p>Foo</p>
                     </div>
-                    <div className="card">
+                    <div className="card" onClick={() => setState(3)}>
                         <img src={img} alt="project3"/>
                         <p>Foo</p>
                     </div>
-                    <div className="card">
+                    <div className="card" onClick={() => setState(4)}>
                         <img src={img} alt="project4"/>
                         <p>Foo</p>
                     </div>
                 </div>
-                { <ProjectModal />}
+                {/* { <ProjectModal />} */}
             </div>
             
         </section>
