3dd61eb47bddccbd3a5d0342ad9793a43d2b871f
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2019-12-19 19:36:12 +0200

    svg anim and google fonts

---
+++ src/components/LogoAnimation.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './LogoAnimation.css';
+
+const LogoAnimation = () => {
+
+    return (
+
+        
+        <div>
+            <svg id='logoT' version="1.1" x="0px" y="0px" width="322.2px"
+                height="357.5px" viewBox="0 0 322.2 357.5">
+                <path className="st0" d="M120.6,353V66.8H4.5V4.5h313.2v62.3H201.6V353H120.6z" />
+                {/* x<path className="st0" d="M70.3,37.4c0,18.2-14.7,32.9-32.9,32.9S4.5,55.6,4.5,37.4S19.2,4.5,37.4,4.5S70.3,19.2,70.3,37.4z" /> */}
+            </svg>
+
+
+
+            {/* <svg id='logoDot' version="1.1" x="0px" y="0px" width="74.8px"
+                height="74.8px" viewBox="0 0 74.8 74.8">
+                
+            </svg> */}
+        </div>
+        
+    );
+}
+
+
+export default LogoAnimation;
+
+
+
+
+
--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from '../portfolioLetterT.png'
 import './MainView.css'
+import LogoAnimation from '../components/LogoAnimation';
 
 const MainView = () => {
     return (
       //  <section id="main">
             <div className="heroSection">
-                <h1>Tom Paavolainen</h1>
+                <h1>TOM PAAVOLAINEN</h1>
                 <div className="animationContainer">
-                    <img src={logo} alt="main logo"/>
+                    <LogoAnimation/>
                 </div>
             </div>
       //  </section>
