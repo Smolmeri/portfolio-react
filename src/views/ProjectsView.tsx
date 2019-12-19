a068ca987bbf1cadfd999124e550add4f0b7e1af
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2019-12-19 13:03:04 +0200

    project images and css fixes

--- src/views/ProjectsView.tsx
+++ src/views/ProjectsView.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './ProjectsView.css';
 import img from '../imgPlaceholder.png'
+import meeKouluun from '../meeKouluun.png';
+import wanderdicks from '../wanderdicks.png';
+import code from '../code.png';
 import ProjectModal from '../components/Projects/ProjectModal';
 
 const ProjectsView = (props) => {
@@ -22,34 +25,34 @@ const ProjectsView = (props) => {
                 <h1>Projects</h1>
                 <div className="cardsSection">
                     <div className="card" onClick={() => setState(1)}>
-                        <img src={img} alt="project1" />
+                        <img src={meeKouluun} alt="project1" />
                         <p>Foo</p>
                     </div>
                     <div className="card" onClick={() => setState(2)}>
-                        <img src={img} alt="project2" />
+                        <img src={wanderdicks} alt="project2" />
                         <p>Foo</p>
                     </div>
                     <div className="card" onClick={() => setState(3)}>
-                        <img src={img} alt="project3" />
+                        <img src={code} alt="project3" />
                         <p>Foo</p>
                     </div>
                     <div className="card" onClick={() => setState(4)}>
-                        <img src={img} alt="project4" />
+                        <img src={code} alt="project4" />
                         <p>Foo</p>
                     </div>
                     
                 </div>
                     {state === 1 &&
-                        <ProjectModal onClick={handleClick} imgPath={img} desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis cum nesciunt impedit fugit amet laudantium voluptatum in, eius ipsa id earum vitae ab blanditiis tenetur eligendi odio eos ad.'} url={'https://github.com/Smolmeri'} urlName={'GITHUB'} />
+                        <ProjectModal onClick={handleClick} imgPath={meeKouluun} desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis cum nesciunt impedit fugit amet laudantium voluptatum in, eius ipsa id earum vitae ab blanditiis tenetur eligendi odio eos ad.'} url={'https://github.com/Smolmeri'} urlName={'GITHUB'} />
                     }
                     {state === 2 &&
-                        <ProjectModal onClick={handleClick} imgPath={img} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis cum nesciunt impedit fugit amet laudantium voluptatum in, eius ipsa id earum vitae ab blanditiis tenetur eligendi odio eos ad."} url={'https://github.com/Smolmeri'} urlName={'GITHUB'} />
+                        <ProjectModal onClick={handleClick} imgPath={wanderdicks} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis cum nesciunt impedit fugit amet laudantium voluptatum in, eius ipsa id earum vitae ab blanditiis tenetur eligendi odio eos ad."} url={'https://github.com/Smolmeri'} urlName={'GITHUB'} />
                     }
                     {state === 3 &&
-                        <ProjectModal onClick={handleClick} imgPath={img} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis cum nesciunt impedit fugit amet laudantium voluptatum in, eius ipsa id earum vitae ab blanditiis tenetur eligendi odio eos ad."} url={'https://github.com/Smolmeri'} urlName={'GITHUB'} />
+                        <ProjectModal onClick={handleClick} imgPath={code} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis cum nesciunt impedit fugit amet laudantium voluptatum in, eius ipsa id earum vitae ab blanditiis tenetur eligendi odio eos ad."} url={'https://github.com/Smolmeri'} urlName={'GITHUB'} />
                     }
                     {state === 4 &&
-                        <ProjectModal onClick={handleClick} imgPath={img} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis cum nesciunt impedit fugit amet laudantium voluptatum in, eius ipsa id earum vitae ab blanditiis tenetur eligendi odio eos ad."} url={'https://github.com/Smolmeri'} urlName={'GITLAB'}/>
+                        <ProjectModal onClick={handleClick} imgPath={code} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis cum nesciunt impedit fugit amet laudantium voluptatum in, eius ipsa id earum vitae ab blanditiis tenetur eligendi odio eos ad."} url={'https://github.com/Smolmeri'} urlName={'GITLAB'}/>
                     }
                 
             </div>
